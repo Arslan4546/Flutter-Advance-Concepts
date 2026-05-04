787c1e195d8e7769837afb57b025cbf5392cdf7e
Arslan4546 <arslantariq4546@gmail.com>
2026-05-04 12:32:39 +0500

    updated

--- auth_practices/auth_practices/lib/main.dart
+++ auth_practices/auth_practices/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:auth_practices/bloc/auth_bloc/auth_bloc.dart';
 import 'package:auth_practices/bloc/auth_bloc/auth_event.dart';
+import 'package:auth_practices/bloc/camera_bloc/camera_bloc.dart';
 import 'package:auth_practices/bloc/phone_auth/phone_auth_bloc.dart';
+import 'package:auth_practices/core/services/camera_service.dart';
 import 'package:auth_practices/routes/app_routes.dart';
 import 'package:auth_practices/data/repositories/auth_repo.dart';
 import 'package:auth_practices/firebase_options.dart';
@@ -11,7 +13,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const AuthApp());
 }
 
@@ -26,6 +27,7 @@ class AuthApp extends StatelessWidget {
           create: (_) => AuthBloc(AuthRepo())..add(const AuthCheckRequested()),
         ),
         BlocProvider(create: (_) => PhoneAuthBloc()),
+        BlocProvider(create: (_) => CameraBloc(CameraService())),
       ],
       child: MaterialApp.router(
         routerConfig: appRouter,
